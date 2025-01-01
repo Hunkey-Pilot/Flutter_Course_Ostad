db078b091520c5b975e6f3e2d628fd673e20223d
Anupam <neelanupam2002@gmail.com>
2025-01-01 23:16:56 +0600

    Updated

---
+++ bin/Triangle_Area.dart
@@ -0,0 +1,15 @@
+import "dart:io";
+
+main(){
+  triangle();
+}
+
+void triangle(){
+   double base,height;
+   print("Enter Base: ");
+   base=double.parse(stdin.readLineSync()!);
+   print("Enter Height: ");
+   height=double.parse(stdin.readLineSync()!);
+
+   print("The Area of the Triangle is ${(.5*base*height).toStringAsFixed(3)} squnit.");
+}
